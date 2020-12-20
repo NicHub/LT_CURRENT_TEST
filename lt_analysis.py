b5574b19a8ad7f275b6e88a39b6443266257915a
NicHub <nicolas.jeanmonod@gmail.com>
2020-12-20 17:10:46 +0100

    Bokeh class

--- lt_analysis.py
+++ lt_analysis.py
@@ -1,55 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+
+LT ANALYSIS
+
+This module shows some basic plotting techniques with Bokeh.
+
+@author         Nicolas Jeanmonod
+@date           2020-02-16
+
+"""
+
+
 import logging
 import sys
 import warnings
 import xml.etree.ElementTree as ET
 
-import bokeh
 import numpy as np
-from bokeh.core.property.color import Color
-from bokeh.io import output_file, show
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, NumeralTickFormatter, Title
-from bokeh.palettes import Turbo256, linear_palette
-from bokeh.plotting import output_file, save, show
+from plot_bokeh import PlotBokeh
+
 
 # User choices.
-TOOLS = "pan, box_zoom, wheel_zoom, save, reset, xzoom_in, xzoom_out"
-PLOT_WIDTH = 1600
-PLOT_HEIGHT = 400
-ALPHA_1 = 1  # 0.1
-ALPHA_6 = 1  # 0.6
-LOGGING_ENABLED = True
-LOGGING_LEVEL = 10
-SHOW_HTML = True
-XML_DIR = "./data/"
-HTML_DIR = "./out_python/"
+SETTINGS = {
+    "GENERAL": {
+        "DATA_DIR": "./data/",
+        "DATA_FILES": ["LT01", "LT30"],
+        "LT_MAX_LEVEL": 400,
+        "PLOT_WIDTH": 1600,
+        "PLOT_HEIGHT": 400,
+        "LOGGING_ENABLED": True,
+        "LOGGING_LEVEL": 10,
+        "SHOW_HTML": True,
+        "COLORS": ("#30123b", "#c0f233", "#3c3285", "#dae236", "#4353c2",
+                   "#f0cb3a", "#4670e8", "#fbb336", "#438efd", "#fd9229",
+                   "#34aaf8", "#f76e1a", "#20c6df", "#ea500d", "#17debf",
+                   "#d73606", "#27eda3", "#c02302", "#4df97c", "#a01101",
+                   "#78fe59", "#7a0402", "#a1fc3d"),
+    },
+    "BOKEH": {
+        "TOOLS": "pan, box_zoom, wheel_zoom, save, reset, xzoom_in, xzoom_out",
+        "ALPHA_1": 1,  # 0.1
+        "ALPHA_6": 1,  # 0.6
+        "CIRCLE_SIZE": 3,
+        "OUT_DIR": "./out_python_bokeh/",
+    }
+}
 
 # Global variables.
-COLORS = []
-LTDATA = {}
-HTML_ELEMS = []
 LOGGER = logging.getLogger(__name__)
-NB_MEAS = None
-NB_HEIGHT = None
 
 
-def readXML(lt_name):
+def read_data(data_file, settings):
     """ ___ """
 
-    global NB_MEAS, NB_HEIGHT
-    file_name = XML_DIR + lt_name + ".xml"
+    file_name = settings["GENERAL"]["DATA_DIR"] + data_file + ".xml"
     tree = ET.parse(file_name)
     root = tree.getroot()
     reshape = True
+    lt_data = {}
+    meas_count = None
+    level_count = None
     for child in root:
         if child.tag in ["ID", "HePressure_mbar"]:
             continue
 
         #
-        # TAGS
+        # XML TAGS
         #
         # Level_mm
         # Voltage_V
@@ -59,282 +77,54 @@ def readXML(lt_name):
         #
         for _elem in root.iter(child.tag):
 
-            NB_MEAS, NB_HEIGHT = _elem.attrib["size"].split(" ")
-            NB_MEAS, NB_HEIGHT = int(NB_MEAS), int(NB_HEIGHT)
+            meas_count, level_count = _elem.attrib["size"].split(" ")
+            meas_count, level_count = int(meas_count), int(level_count)
 
             if reshape:
-                LTDATA[child.tag] = np.asarray(_elem.text.split(" ")).astype(
-                    "float64").reshape(NB_HEIGHT, NB_MEAS)
+                lt_data[child.tag] = np.asarray(_elem.text.split(" ")).astype(
+                    "float64").reshape(level_count, meas_count)
             else:
-                LTDATA[child.tag] = np.asarray(
+                lt_data[child.tag] = np.asarray(
                     _elem.text.split(" ")).astype("float64")
 
     if reshape:
-        LTDATA["KeithleyTimeStamp"] -= LTDATA["KeithleyTimeStamp"][0][0]
+        lt_data["KeithleyTimeStamp"] -= lt_data["KeithleyTimeStamp"][0][0]
     else:
-        LTDATA["KeithleyTimeStamp"] -= LTDATA["KeithleyTimeStamp"][0]
+        lt_data["KeithleyTimeStamp"] -= lt_data["KeithleyTimeStamp"][0]
 
-    global COLORS
-    better_contrast = False
-    if better_contrast:
-        COLORS = (
-            "#30123b",
-            "#c0f233",
-            "#3c3285",
-            "#dae236",
-            "#4353c2",
-            "#f0cb3a",
-            "#4670e8",
-            "#fbb336",
-            "#438efd",
-            "#fd9229",
-            "#34aaf8",
-            "#f76e1a",
-            "#20c6df",
-            "#ea500d",
-            "#17debf",
-            "#d73606",
-            "#27eda3",
-            "#c02302",
-            "#4df97c",
-            "#a01101",
-            "#78fe59",
-            "#7a0402",
-            "#a1fc3d",
-        )
-    else:
-        COLORS = linear_palette(Turbo256, NB_HEIGHT)
+    data = {
+        "lt_data": lt_data,
+        "lt_name": data_file,
+        "file_name": file_name,
+        "meas_count": meas_count,
+        "level_count": level_count,
+    }
+
+    return data
 
 
-def calc_resistivity(lt_name):
+def calc_resistivity(data, settings):
     """ ___ """
 
-    max_length = 400
-
     # Suppress “divide by zero” warning.
+    # This is a very bad approch. Better would be to check
+    # when `levels >= LT_MAX_LEVEL` and return `resistivity = NaN`.
+    # TODO: Check `levels >= LT_MAX_LEVEL`.
     with warnings.catch_warnings(record=True) as _w:
         warnings.simplefilter("always")
-        LTDATA["resistivity"] = LTDATA["Resistance_ohm"] / \
-            (max_length - LTDATA["Level_mm"])
+        data["lt_data"]["resistivity"] = data["lt_data"]["Resistance_ohm"] / \
+            (settings["GENERAL"]["LT_MAX_LEVEL"] - data["lt_data"]["Level_mm"])
         if len(_w) == 1 \
                 and issubclass(_w[-1].category, RuntimeWarning) \
                 and "divide by zero" in str(_w[-1].message):
             LOGGER.debug(
                 '%s — Some resistivity values cannot be calculated'
-                ' because "Level = Max Height"', lt_name)
+                ' because "Level = Max Height"', data["lt_name"])
+
+    return data
 
 
-def plot_current_vs_time(lt_name):
-    """ ___ """
-
-    #
-    # Format plot.
-    #
-    plt = bokeh.plotting.figure(tools=TOOLS)
-    plt.toolbar.logo = None
-    plt.plot_width = PLOT_WIDTH
-    plt.plot_height = PLOT_HEIGHT
-    plt.add_layout(
-        Title(
-            text=f"{lt_name} — Current vs time",
-            text_font_style="normal",
-            align="center"),
-        "above")
-    plt.xaxis.axis_label = "Time (s)"
-    plt.yaxis.axis_label = "Current (A)"
-    plt.yaxis.formatter = NumeralTickFormatter(format="0.000")
-
-    #
-    # Prepare data source and plot data.
-    #
-    for _h in range(NB_HEIGHT):
-        _t = LTDATA["KeithleyTimeStamp"][_h]
-        _y = LTDATA["Current_A"][_h]
-        data_source = ColumnDataSource(data=dict(t=_t, y=_y))
-
-        plt.line("t", "y", source=data_source, color=COLORS[_h],
-                 legend_label="Current (A)", alpha=ALPHA_1)
-        plt.circle("t", "y", source=data_source, color=COLORS[_h],
-                   size=3, alpha=ALPHA_6)
-
-    #
-    # Append plot to HTML elements for final report.
-    #
-    HTML_ELEMS.append(plt)
-
-
-def plot_resistance_vs_time(lt_name):
-    """ ___ """
-
-    #
-    # Format plot.
-    #
-    plt = bokeh.plotting.figure(tools=TOOLS)
-    plt.toolbar.logo = None
-    plt.plot_width = PLOT_WIDTH
-    plt.plot_height = PLOT_HEIGHT
-    plt.add_layout(
-        Title(
-            text=f"{lt_name} — Resistance vs time",
-            text_font_style="normal",
-            align="center"),
-        "above")
-    plt.xaxis.axis_label = "Time (s)"
-    plt.yaxis.axis_label = "Resistance (Ω)"
-    plt.yaxis.formatter = NumeralTickFormatter(format="0")
-    plt.x_range = HTML_ELEMS[0].x_range
-
-    #
-    # Prepare data source and plot data.
-    #
-    for _h in range(NB_HEIGHT):
-        _t = LTDATA["KeithleyTimeStamp"][_h]
-        _y = LTDATA["Resistance_ohm"][_h]
-        data_source = ColumnDataSource(data=dict(t=_t, y=_y))
-
-        plt.line("t", "y", source=data_source, color=COLORS[_h],
-                 legend_label="Resistance (Ω)", alpha=ALPHA_1)
-        plt.circle("t", "y", source=data_source, color=COLORS[_h],
-                   size=3, alpha=ALPHA_6)
-
-    #
-    # Append plot to HTML elements for final report.
-    #
-    HTML_ELEMS.append(plt)
-
-
-def plot_level_vs_time(lt_name):
-    """ ___ """
-
-    #
-    # Format plot.
-    #
-    plt = bokeh.plotting.figure(tools=TOOLS)
-    plt.toolbar.logo = None
-    plt.plot_width = PLOT_WIDTH
-    plt.plot_height = PLOT_HEIGHT
-    plt.add_layout(
-        Title(
-            text=f"{lt_name} — Level vs time",
-            text_font_style="normal",
-            align="center"),
-        "above")
-    plt.xaxis.axis_label = "Time (s)"
-    plt.yaxis.axis_label = "Level (mm)"
-    plt.yaxis.formatter = NumeralTickFormatter(format="0")
-    plt.x_range = HTML_ELEMS[0].x_range
-
-    #
-    # Prepare data source and plot data.
-    #
-    for _h in range(NB_HEIGHT):
-        _t = LTDATA["KeithleyTimeStamp"][_h]
-        _y = LTDATA["Level_mm"][_h]
-        data_source = ColumnDataSource(data=dict(t=_t, y=_y))
-
-        plt.line("t", "y", source=data_source, color=COLORS[_h],
-                 legend_label="Level (mm)", alpha=ALPHA_1)
-        plt.circle("t", "y", source=data_source, color=COLORS[_h],
-                   size=3, alpha=ALPHA_6)
-
-    #
-    # Append plot to HTML elements for final report.
-    #
-    HTML_ELEMS.append(plt)
-
-
-def plot_resistivity_vs_time(lt_name):
-    """ ___ """
-
-    #
-    # Format plot.
-    #
-    plt = bokeh.plotting.figure(tools=TOOLS)
-    plt.toolbar.logo = None
-    plt.plot_width = PLOT_WIDTH
-    plt.plot_height = PLOT_HEIGHT
-    plt.add_layout(
-        Title(
-            text=f"{lt_name} — Resistivity vs time",
-            text_font_style="normal",
-            align="center"),
-        "above")
-    plt.xaxis.axis_label = "Time (s)"
-    plt.yaxis.axis_label = "Resistivity (Ω/mm)"
-    plt.yaxis.formatter = NumeralTickFormatter(format="0.000")
-    plt.x_range = HTML_ELEMS[0].x_range
-
-    #
-    # Prepare data source and plot data.
-    #
-    for _h in range(NB_HEIGHT):
-        _t = LTDATA["KeithleyTimeStamp"][_h]
-        _y = LTDATA["resistivity"][_h]
-        data_source = ColumnDataSource(data=dict(t=_t, y=_y))
-
-        plt.line("t", "y", source=data_source, color=COLORS[_h],
-                 legend_label="Resistivity (Ω/mm)", alpha=ALPHA_1)
-        plt.circle("t", "y", source=data_source, color=COLORS[_h],
-                   size=3, alpha=ALPHA_6)
-
-    #
-    # Append plot to HTML elements for final report.
-    #
-    HTML_ELEMS.append(plt)
-
-
-def plot_resistance_vs_current(lt_name):
-    """ ___ """
-
-    #
-    # Format plot.
-    #
-    plt = bokeh.plotting.figure(tools=TOOLS)
-    plt.toolbar.logo = None
-    plt.plot_width = PLOT_WIDTH
-    plt.plot_height = PLOT_WIDTH
-    plt.add_layout(
-        Title(
-            text=f"{lt_name} — Resistance vs current",
-            text_font_style="normal",
-            align="center"),
-        "above")
-    plt.xaxis.axis_label = "Current (A)"
-    plt.yaxis.axis_label = "Resistance (Ω)"
-    plt.yaxis.formatter = NumeralTickFormatter(format="0")
-
-    #
-    # Prepare data source and plot data.
-    #
-    for _h in range(NB_HEIGHT):
-        _i = LTDATA["Current_A"][_h]
-        _r = LTDATA["Resistance_ohm"][_h]
-        data_source = ColumnDataSource(data=dict(i=_i, r=_r))
-
-        plt.line("i", "r", source=data_source, color=COLORS[_h],
-                 legend_label=f'R(I) @ L{LTDATA["Level_mm"][_h][0]:0.0f}mm (Ω)', alpha=ALPHA_6)
-        plt.circle("i", "r", source=data_source, color=COLORS[_h],
-                   size=3, alpha=ALPHA_1)
-
-    #
-    # Append plot to HTML elements for final report.
-    #
-    HTML_ELEMS.append(plt)
-
-
-def write_to_html_file(lt_name):
-    """ ___ """
-
-    file_name = HTML_DIR + lt_name + ".html"
-    output_file(file_name, title=f"{lt_name}")
-    _c = column(children=HTML_ELEMS, sizing_mode="scale_width")
-    if SHOW_HTML:
-        show(_c)
-    else:
-        save(_c)
-
-
-def init_logger():
+def init_logger(settings):
     """
     50 CRITICAL, 40 ERROR, 30 WARNING, 20 INFO, 10 DEBUG, 0 NOTSET
 
@@ -344,7 +134,7 @@ def init_logger():
     print(loggers)
     """
 
-    if not LOGGING_ENABLED:
+    if not settings["GENERAL"]["LOGGING_ENABLED"]:
         logging.disable(50)
         return
 
@@ -357,36 +147,56 @@ def init_logger():
                         "%(message)s",
                         datefmt="%Y-%m-%d %H:%M:%S")
 
-    LOGGER.setLevel(LOGGING_LEVEL)
+    LOGGER.setLevel(settings["GENERAL"]["LOGGING_LEVEL"])
+    logging.getLogger("plot_bokeh").setLevel(
+        settings["GENERAL"]["LOGGING_LEVEL"])
 
     LOGGER.debug("python %s", sys.version.split(" ")[0])
-    LOGGER.debug("bokeh %s", bokeh.__version__)
     LOGGER.debug("numpy %s", np.__version__)
 
 
-def process_lts(lt_name):
+def plot_with_bokeh(settings, data):
     """ ___ """
 
-    LOGGER.debug("Processing %s", lt_name)
-    readXML(lt_name)
-    calc_resistivity(lt_name)
-    plot_current_vs_time(lt_name)
-    plot_resistance_vs_time(lt_name)
-    plot_level_vs_time(lt_name)
-    plot_resistivity_vs_time(lt_name)
-    plot_resistance_vs_current(lt_name)
-    write_to_html_file(lt_name)
+    plotb = PlotBokeh(settings, data)
+    plotb.title()
+    plotb.plot_current_vs_time()
+    plotb.plot_resistance_vs_time()
+    plotb.plot_level_vs_time()
+    plotb.plot_resistivity_vs_time()
+    plotb.plot_resistance_vs_current()
+    plotb.write_to_html_file()
+
+
+def read_settings():
+    """
+    In this simple example, the parameters are stored in this file.
+    In a production environment, they would be stored externally.
+
+    TODO: Perform sanity checks.
+    """
+
+    settings = SETTINGS
+    return settings
 
 
 def main():
     """ ___ """
 
-    init_logger()
-    lt_names = ["LT01", "LT30"]
-    global HTML_ELEMS
-    for _lt_name in lt_names:
-        HTML_ELEMS = []
-        process_lts(_lt_name)
+    # Init.
+    settings = read_settings()
+    init_logger(settings)
+
+    # Process data files.
+    for data_file in settings["GENERAL"]["DATA_FILES"]:
+
+        # Read data and calculate resistivity.
+        LOGGER.debug("Processing %s", data_file)
+        data = read_data(data_file, settings)
+        data = calc_resistivity(data, settings)
+
+        # Plot with Bokeh.
+        plot_with_bokeh(settings, data)
 
 
 if __name__ == "__main__":
---
+++ plot_bokeh.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+PLOT BOKEH
+
+This module shows some basic plotting techniques with Bokeh.
+
+@author         Nicolas Jeanmonod
+@date           2020-12-20
+
+"""
+
+
+import logging
+import os
+
+import bokeh
+from bokeh.layouts import column, row, gridplot
+from bokeh.models import ColumnDataSource, NumeralTickFormatter, Title
+from bokeh.plotting import figure, output_file, save, show
+from bokeh.models.widgets import Div
+
+
+class PlotBokeh():
+    """ ___ """
+
+    def __init__(self, settings, data):
+        """ ___ """
+
+        self.__settings = settings
+        self.__data = data
+        self.__html_elems = []
+        self.__plot_margin = (20, 100, 20, 100)
+
+        # ID of the plot that is used for common x_range.
+        self.__master_x_range = 1
+
+        self.__logger = logging.getLogger(__name__)
+        self.__logger.debug("bokeh %s", bokeh.__version__)
+
+    def title(self):
+        """ ___ """
+
+        title = Div(
+            text=f"""
+        <h1 style="
+            text-align: center;
+            font-weight: 100;
+            text-transform: uppercase;
+            font-family: "Ubuntu Mono", monospace;
+            >
+            {self.__data["lt_name"]} Measurements
+        </h1>""",
+            sizing_mode="stretch_width",
+            height=100)
+
+        title.margin = self.__plot_margin
+
+        # title = row(Div)
+        self.__html_elems.append(title)
+
+    def plot_current_vs_time(self):
+        """ ___ """
+
+        #
+        # Format plot.
+        #
+        plt = figure(tools=self.__settings["BOKEH"]["TOOLS"])
+        plt.toolbar.logo = None
+        plt.plot_width = self.__settings["GENERAL"]["PLOT_WIDTH"]
+        plt.plot_height = self.__settings["GENERAL"]["PLOT_HEIGHT"]
+        plt.add_layout(
+            Title(
+                text=f'{self.__data["lt_name"]} — Current vs time',
+                text_font_style="normal",
+                align="center"),
+            "above")
+        plt.xaxis.axis_label = "Time (s)"
+        plt.yaxis.axis_label = "Current (A)"
+        plt.yaxis.formatter = NumeralTickFormatter(format="0.000")
+        plt.margin = self.__plot_margin
+
+        #
+        # Prepare data source and plot data.
+        #
+        for level in range(self.__data["level_count"]):
+            _t = self.__data["lt_data"]["KeithleyTimeStamp"][level]
+            _y = self.__data["lt_data"]["Current_A"][level]
+            data_source = ColumnDataSource(data=dict(t=_t, y=_y))
+
+            plt.line("t", "y", source=data_source,
+                     color=self.__settings["GENERAL"]["COLORS"][level],
+                     legend_label="Current (A)",
+                     alpha=self.__settings["BOKEH"]["ALPHA_1"])
+            plt.circle("t", "y", source=data_source,
+                       color=self.__settings["GENERAL"]["COLORS"][level],
+                       size=self.__settings["BOKEH"]["CIRCLE_SIZE"],
+                       alpha=self.__settings["BOKEH"]["ALPHA_6"])
+
+        #
+        # Append plot to HTML elements for final report.
+        #
+        self.__html_elems.append(plt)
+
+    def plot_resistance_vs_time(self):
+        """ ___ """
+
+        #
+        # Format plot.
+        #
+        plt = figure(tools=self.__settings["BOKEH"]["TOOLS"])
+        plt.toolbar.logo = None
+        plt.plot_width = self.__settings["GENERAL"]["PLOT_WIDTH"]
+        plt.plot_height = self.__settings["GENERAL"]["PLOT_HEIGHT"]
+        plt.add_layout(
+            Title(
+                text=f'{self.__data["lt_name"]} — Resistance vs time',
+                text_font_style="normal",
+                align="center"),
+            "above")
+        plt.xaxis.axis_label = "Time (s)"
+        plt.yaxis.axis_label = "Resistance (Ω)"
+        plt.yaxis.formatter = NumeralTickFormatter(format="0")
+        plt.x_range = self.__html_elems[self.__master_x_range].x_range
+        plt.margin = self.__plot_margin
+
+        #
+        # Prepare data source and plot data.
+        #
+        for level in range(self.__data["level_count"]):
+            _t = self.__data["lt_data"]["KeithleyTimeStamp"][level]
+            _y = self.__data["lt_data"]["Resistance_ohm"][level]
+            data_source = ColumnDataSource(data=dict(t=_t, y=_y))
+
+            plt.line("t", "y", source=data_source,
+                     color=self.__settings["GENERAL"]["COLORS"][level],
+                     legend_label="Resistance (Ω)",
+                     alpha=self.__settings["BOKEH"]["ALPHA_1"])
+            plt.circle("t", "y", source=data_source,
+                       color=self.__settings["GENERAL"]["COLORS"][level],
+                       size=self.__settings["BOKEH"]["CIRCLE_SIZE"],
+                       alpha=self.__settings["BOKEH"]["ALPHA_6"])
+
+        #
+        # Append plot to HTML elements for final report.
+        #
+        self.__html_elems.append(plt)
+
+    def plot_level_vs_time(self):
+        """ ___ """
+
+        #
+        # Format plot.
+        #
+        plt = figure(tools=self.__settings["BOKEH"]["TOOLS"])
+        plt.toolbar.logo = None
+        plt.plot_width = self.__settings["GENERAL"]["PLOT_WIDTH"]
+        plt.plot_height = self.__settings["GENERAL"]["PLOT_HEIGHT"]
+        plt.add_layout(
+            Title(
+                text=f'{self.__data["lt_name"]} — Level vs time',
+                text_font_style="normal",
+                align="center"),
+            "above")
+        plt.xaxis.axis_label = "Time (s)"
+        plt.yaxis.axis_label = "Level (mm)"
+        plt.yaxis.formatter = NumeralTickFormatter(format="0")
+        plt.x_range = self.__html_elems[self.__master_x_range].x_range
+        plt.margin = self.__plot_margin
+
+        #
+        # Prepare data source and plot data.
+        #
+        for level in range(self.__data["level_count"]):
+            _t = self.__data["lt_data"]["KeithleyTimeStamp"][level]
+            _y = self.__data["lt_data"]["Level_mm"][level]
+            data_source = ColumnDataSource(data=dict(t=_t, y=_y))
+
+            plt.line("t", "y", source=data_source,
+                     color=self.__settings["GENERAL"]["COLORS"][level],
+                     legend_label="Level (mm)",
+                     alpha=self.__settings["BOKEH"]["ALPHA_1"])
+            plt.circle("t", "y", source=data_source,
+                       color=self.__settings["GENERAL"]["COLORS"][level],
+                       size=self.__settings["BOKEH"]["CIRCLE_SIZE"],
+                       alpha=self.__settings["BOKEH"]["ALPHA_6"])
+
+        #
+        # Append plot to HTML elements for final report.
+        #
+        self.__html_elems.append(plt)
+
+    def plot_resistivity_vs_time(self):
+        """ ___ """
+
+        #
+        # Format plot.
+        #
+        plt = figure(tools=self.__settings["BOKEH"]["TOOLS"])
+        plt.toolbar.logo = None
+        plt.plot_width = self.__settings["GENERAL"]["PLOT_WIDTH"]
+        plt.plot_height = self.__settings["GENERAL"]["PLOT_HEIGHT"]
+        plt.add_layout(
+            Title(
+                text=f'{self.__data["lt_name"]} — Resistivity vs time',
+                text_font_style="normal",
+                align="center"),
+            "above")
+        plt.xaxis.axis_label = "Time (s)"
+        plt.yaxis.axis_label = "Resistivity (Ω/mm)"
+        plt.yaxis.formatter = NumeralTickFormatter(format="0.000")
+        plt.x_range = self.__html_elems[self.__master_x_range].x_range
+        plt.margin = self.__plot_margin
+
+        #
+        # Prepare data source and plot data.
+        #
+        for level in range(self.__data["level_count"]):
+            _t = self.__data["lt_data"]["KeithleyTimeStamp"][level]
+            _y = self.__data["lt_data"]["resistivity"][level]
+            data_source = ColumnDataSource(data=dict(t=_t, y=_y))
+
+            plt.line("t", "y", source=data_source,
+                     color=self.__settings["GENERAL"]["COLORS"][level],
+                     legend_label="Resistivity (Ω/mm)",
+                     alpha=self.__settings["BOKEH"]["ALPHA_1"])
+            plt.circle("t", "y", source=data_source,
+                       color=self.__settings["GENERAL"]["COLORS"][level],
+                       size=self.__settings["BOKEH"]["CIRCLE_SIZE"],
+                       alpha=self.__settings["BOKEH"]["ALPHA_6"])
+
+        #
+        # Append plot to HTML elements for final report.
+        #
+        self.__html_elems.append(plt)
+
+    def plot_resistance_vs_current(self):
+        """ ___ """
+
+        #
+        # Create plot.
+        #
+        plt = figure(tools=self.__settings["BOKEH"]["TOOLS"])
+
+        #
+        # Prepare data source and plot data.
+        #
+        for level in range(self.__data["level_count"]):
+            _i = self.__data["lt_data"]["Current_A"][level]
+            _r = self.__data["lt_data"]["Resistance_ohm"][level]
+            data_source = ColumnDataSource(data=dict(i=_i, r=_r))
+
+            level_val = self.__data["lt_data"]["Level_mm"][level][0]
+            legend_label = f'R(I) @ L{level_val:0.0f}mm (Ω)'
+            plt.line("i", "r", source=data_source,
+                     color=self.__settings["GENERAL"]["COLORS"][level],
+                     legend_label=legend_label,
+                     alpha=self.__settings["BOKEH"]["ALPHA_6"])
+            plt.circle("i", "r", source=data_source,
+                       color=self.__settings["GENERAL"]["COLORS"][level],
+                       legend_label=legend_label,
+                       size=self.__settings["BOKEH"]["CIRCLE_SIZE"],
+                       alpha=self.__settings["BOKEH"]["ALPHA_1"])
+
+        #
+        # Format plot.
+        #
+        plt.toolbar.logo = None
+        plt.plot_width = self.__settings["GENERAL"]["PLOT_WIDTH"]
+        plt.plot_height = self.__settings["GENERAL"]["PLOT_WIDTH"]
+        plt.add_layout(
+            Title(
+                text=f'{self.__data["lt_name"]} — Resistance vs current',
+                text_font_style="normal",
+                align="center"),
+            "above")
+        plt.xaxis.axis_label = "Current (A)"
+        plt.yaxis.axis_label = "Resistance (Ω)"
+        plt.yaxis.formatter = NumeralTickFormatter(format="0")
+        plt.margin = self.__plot_margin
+        plt.legend.location = "top_left"
+        plt.legend.click_policy = "hide"
+
+        #
+        # Append plot to HTML elements for final report.
+        #
+        self.__html_elems.append(plt)
+
+    def write_to_html_file(self):
+        """ ___ """
+
+        if not os.path.exists(self.__settings["BOKEH"]["OUT_DIR"]):
+            self.__logger.debug("Creating output dir %s",
+                                self.__settings["BOKEH"]["OUT_DIR"])
+            os.mkdir(self.__settings["BOKEH"]["OUT_DIR"])
+
+        file_name = self.__settings["BOKEH"]["OUT_DIR"] + \
+            self.__data["lt_name"] + ".html"
+        output_file(file_name, title=f'{self.__data["lt_name"]}')
+
+        html_out = column(children=self.__html_elems,
+                          sizing_mode="stretch_width")
+
+        # html_out = gridplot(self.__html_elems, ncols=1, sizing_mode="stretch_both")
+
+        if self.__settings["GENERAL"]["SHOW_HTML"]:
+            show(html_out)
+        else:
+            save(html_out)
